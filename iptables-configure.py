d73e97ff6509d740ec37325a6600ba715c2e8752
Kyle Misteld <kmistele@smu.edu>
2019-12-24 22:08:31 -0600

    code to configure default policies

--- iptables-configure.py
+++ iptables-configure.py
@@ -1,4 +1,5 @@
 import time, os, sys, shutil
+
 print("Blackl1ght presents...")
 time.sleep(1.5)
 print(
@@ -36,7 +37,61 @@ if (os.name == 'nt'):
 # GET PATH OF IPTABLES BINARY
 if (shutil.which('iptables') != str):
     sys.exit("\nIPTables is not in the PATH.")
+
+# MAKE SURE THAT THE USER IS RUNNING THE SCRIPT AS ROOT SO THAT WE CAN ACTUALLY MODIFY IPTABLES RULES
 if not os.geteuid() == 0:
     sys.exit("\nPlease run this script with SUDO or as root.\n")
 
+# CREATE THE FILE; WILL OVERWRITE IF IT ALREADY EXISTS
+script = open("rules.sh", "w+")
+script.write("#!bin/bash")
 
+# FLUSH IPTABLES RULES?
+choice = input("[*] Flush existing IPTables rules? (recommended) Y/N: ")
+while (choice.lower() != 'y' and choice.lower() != 'n'):
+    choice = input("[*] Flush existing IPTables rules? (recommended) Y/N: ")
+
+if (choice.lower() == 'y'):
+    script.write("# FLUSH IPTABLES RULES\n")
+    script.write("iptables -F\n")
+    script.write("iptables -X\n")
+    script.write("iptables -t nat -F\n")
+    script.write("iptables -t nat -X\n")
+    script.write("iptables -t mangle -F\n")
+    script.write("iptables -t mangle -X\n")
+    script.write("\n")
+
+##############################################
+# SET DEFAULT POLICIES
+##############################################
+
+script.write("# DEFAULT POLICIES\n")
+
+# INPUT POLICY
+choice = input("[*] Set default INPUT policy to DROP? (recommended) Y/N: ")
+while(choice.lower() != 'y' and choice.lower() != 'n'):
+    choice = input("[*] Set default INPUT policy to DROP? (recommended) Y/N: ")
+
+if (choice.lower() == 'y'):
+    script.write("iptables -P INPUT DROP\n")
+
+# OUTPUT POLICY
+choice = input("[*] Set default OUTPUT policy to DROP? (recommended) Y/N: ")
+while(choice.lower() != 'y' and choice.lower() != 'n'):
+    choice = input("[*] Set default OUTPUT policy to DROP? (recommended) Y/N: ")
+
+if (choice.lower() == 'y'):
+    script.write("iptables -P OUTPUT DROP\n")
+
+# FORWARD POLICY
+choice = input("[*] Set default FORWARD policy to DROP? (recommended) Y/N: ")
+while(choice.lower() != 'y' and choice.lower() != 'n'):
+    choice = input("[*] Set default FORWARD policy to DROP? (recommended) Y/N: ")
+
+if (choice.lower() == 'y'):
+    script.write("iptables -P FORWARD DROP\n")
+
+#############################################################
+# ALL OPTIONS SHOULD GO BEFORE HERE WHERE THE FILE IS CLOSED
+#############################################################
+script.close()
