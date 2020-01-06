5a3952eb165c747ca5a839872f2487fce8b839db
Kyle Misteld <kmistele@smu.edu>
2020-01-05 21:01:03 -0500

    added more clearing to clear-rules.sh

--- clear-rules.sh
+++ clear-rules.sh
@@ -6,9 +6,13 @@ echo clearing rules!
 iptables -F
 iptables -X
 iptables -t nat -F
+iptables -t nat -X
 iptables -t mangle -F
+iptables -t mangle -X
 
 echo reseting default policies for input, output, and forward chains!
+
+# RESET DEFAULT POLICIES
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
